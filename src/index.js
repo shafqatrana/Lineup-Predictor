68375a80e742e43d1a95ddd6a2a791c17cd29d62
Shafqat Rana <shafqatrana2000@gmail.com>
2022-09-17 00:23:46 -0500

    Added file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styling/Master.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
