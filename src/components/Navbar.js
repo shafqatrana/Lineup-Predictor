a21f8c4b808c34fd29fa24188b9a870dc2b9812d
Shafqat Rana <shafqatrana2000@gmail.com>
2022-12-24 20:35:30 -0600

    Added navbar and some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import "../styling/Navbar.css"
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <Link to="/" className="site-title">
-        Site Name
-      </Link>
-      <ul>
-        <CustomLink to="/account">Account</CustomLink>
-      </ul>
-    </nav>
+    <div className="navigation-bar">
+       <nav className="nav">
+          <div className="nav-option"> <Link to="/" className="site-title"> Site Name </Link> </div>
+          <div className="nav-option"> <CustomLink to="/account">Account</CustomLink> </div>
+          <div className="nav-option"> <CustomLink to="/account">Leaderboard</CustomLink> </div>
+       </nav>
+    </div>
+    
   )
 }
 
