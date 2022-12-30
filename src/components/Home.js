479140e12f83bd7421c5661a1470a837e60ebd6b
Shafqat Rana <shafqatrana2000@gmail.com>
2022-12-30 00:35:43 -0600

    Added home page and fixed navbar routing

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../contexts/AuthContext';
+
+const Home = () => {
+
+  return (
+    <div className="account">
+      TEST
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath, Outlet } from "react-router-dom"
 import "../styling/Navbar.css"
 
 export default function Navbar() {
   return (
-    <div className="navigation-bar">
-       <nav className="nav">
-          <div className="nav-option"> <Link to="/" className="site-title"> Site Name </Link> </div>
-          <div className="nav-option"> <CustomLink to="/account">Account</CustomLink> </div>
-          <div className="nav-option"> <CustomLink to="/account">Leaderboard</CustomLink> </div>
-       </nav>
+    <div>
+      <div className="navigation-bar">
+        <nav className="nav">
+            <div className="nav-option"> <Link to="/home" className="site-title"> Site Name </Link> </div>
+            <div className="nav-option"> <CustomLink to="/account">Account</CustomLink> </div>
+            <div className="nav-option"> <CustomLink to="/account">Leaderboard</CustomLink> </div>
+        </nav>
+      </div>
+       <Outlet>
+       </Outlet>
     </div>
-    
   )
 }
 
